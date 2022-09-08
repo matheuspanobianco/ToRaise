29f59d66ab41384975344af0194cfc56ed9ba40d
Matheus Panobianco <matheuspanobianco@hotmail.com>
2022-09-07 21:49:38 -0300

    Primeiro commit

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace FuncaoElevado
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Difícil 27. Escreva uma função que recebe, por parâmetro, dois valores X e Z e calcula e retorna X^Z . (sem utilizar funções ou operadores de potência prontos)
+
+            Console.WriteLine("Escreva dois valores, X e Z, para que receba o valor X^Z");
+            double valorX = double.Parse(Console.ReadLine());
+            double valorZ = double.Parse(Console.ReadLine());
+            double valorXZ;
+        
+            switch (valorZ){
+                case 0:
+                {
+                    valorXZ = 1;
+                    Console.WriteLine($"X^Z = {valorXZ}");
+                    break;
+                }
+                
+                case > 0:
+                {
+                    valorXZ = valorX;
+                    for(int i = 2; i<=valorZ; i++)
+                    {
+                        if(valorZ==1)
+                        {
+                            valorXZ = valorX;
+                            Console.WriteLine($"X^Z = {valorXZ}");
+                            return;            
+                        }
+                        valorXZ *= valorX;
+                    }
+                    Console.WriteLine($"X^Z = {valorXZ}");
+                    break;
+                }
+                
+                case < 0:   
+                {
+                    valorXZ = 1d/valorX;
+                    for(int i = -2; i>=valorZ; i--)
+                    {
+                        if(valorZ==-1)
+                        {
+                            valorXZ = 1d/valorX;
+                            Console.WriteLine($"X^Z = {valorXZ}");
+                            return; 
+                        }
+                        valorXZ *= 1d/valorX;
+                    }
+                    Console.WriteLine($"X^Z = {valorXZ}");
+                    break;
+                }
+            }
+        }
+    }
+}
